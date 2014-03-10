6a2093d68e71ff0a0a90ca4ee292d268ec4ae6b6
anamani <anamani.info@gmail.com>
2014-03-10 19:19:05 +0100

    first commit
    start

---
+++ MasterBrick/src/Fenetre.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class Fenetre extends JFrame{
+	
+	public Fenetre(){
+		
+		JPanel pan = new JPanel();
+		
+		this.setTitle("Ton cul");
+		this.setSize(1000, 800);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+		
+		this.setContentPane(pan);
+	}
+}
